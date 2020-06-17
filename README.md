c027799f437825421bff81a48e78ac125af5b502
MarzanShuvo <marzanalam3@gmail.com>
2020-06-18 03:26:51 +0600

    modified

---
+++ README.md
@@ -0,0 +1,16 @@
+# State Estimation and Localization for Self-Driving Cars
+
+>This course helped me to :
+
+* Understand the key methods for parameter and state estimation used for autonomous driving, such as the method of least-squares
+
+* Develop a model for typical vehicle localization sensors, including GPS and IMUs
+
+* Apply extended and unscented Kalman Filters to a vehicle state estimation problem
+
+* Understand LIDAR scan matching and the Iterative Closest Point algorithm 
+
+* Apply these tools to fuse multiple sensor streams into a single state estimate for a self-driving car
+
+
+
---
+++ coursera_final/test.py
@@ -0,0 +1,23 @@
+# Starter code for the Coursera SDC Course 2 final project.
+#
+# Author: Trevor Ablett and Jonathan Kelly
+# University of Toronto Institute for Aerospace Studies
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from rotations import angle_normalize, rpy_jacobian_axis_angle, skew_symmetric, Quaternion
+
+#### 1. Data ###################################################################################
+
+################################################################################################
+# This is where you will load the data from the pickle files. For parts 1 and 2, you will use
+# p1_data.pkl. For Part 3, you will use pt3_data.pkl.
+################################################################################################
+with open('data/pt1_data.pkl', 'rb') as file:
+    data = pickle.load(file)
+
+imu_f = data['imu_f']
+imu_w = data['imu_w']
+
+print(imu_f.data)
